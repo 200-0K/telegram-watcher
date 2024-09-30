7a18193db2b5e7d754bdf61b822317b354eeb58a
Faisal <faisal.s.alnamlah@gmail.com>
2024-09-30 17:57:55 +0300

    constants and telegram setup

---
+++ src/notifiers/telegram.ts
@@ -0,0 +1,22 @@
+import TelegramBot from "node-telegram-bot-api";
+import { TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID } from "../constants";
+import { Notifier } from "../interfaces/notifier";
+
+if (!TELEGRAM_BOT_TOKEN || !TELEGRAM_CHAT_ID) {
+  console.log('TELEGRAM_BOT_TOKEN is not provided');
+  process.exit(1);
+}
+
+const bot = new TelegramBot(TELEGRAM_BOT_TOKEN);
+
+export const telegram: Notifier = {
+  send(message: string) {
+    bot.sendMessage(
+      TELEGRAM_CHAT_ID!,
+      message,
+      {
+        parse_mode: 'Markdown'
+      }
+    );
+  }
+}
