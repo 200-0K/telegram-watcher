07ceefe219193569eeb30f6c88714fab8c6fc737
Faisal <faisal.s.alnamlah@gmail.com>
2024-10-02 20:58:49 +0300

    Error handling

--- src/index.ts
+++ src/index.ts
@@ -4,19 +4,23 @@ import { report } from "./helpers/notifier";
 import { watchers } from "./watchers";
 
 for (const watcher of watchers) {
-  const response = await fetch(watcher.url, {
-    method: watcher.requestType ?? 'GET',
-    headers: await watcher.headers?.(),
-  });
-  const text = await response.text();
+  try {
+    const response = await fetch(watcher.url, {
+      method: watcher.requestType ?? 'GET',
+      headers: await watcher.headers?.(),
+    });
+    const text = await response.text();
 
-  let data: any = text;
-  if (watcher.responseType == 'json') {
-    data = parseJson(text);
-  }
+    let data: any = text;
+    if (watcher.responseType == 'json') {
+      data = parseJson(text);
+    }
 
-  const message = watcher.notify(data, response.status);
-  if (message) {
-    report(watcher.name, message);
+    const message = watcher.notify(data, response.status);
+    if (message) {
+      report(watcher.name, message);
+    }
+  } catch (e) {
+    report(watcher.name, `Error:\n\`\`\`\n${e}\n\`\`\``);
   }
 }
